387d9b36b77b7d9c8ff5b1286526a5c2564667dc
BillySmithDesign <josh@bullsharkinvestments.com>
2024-06-30 01:51:51 +0930

    1.0

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const logger = require('./logger');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
